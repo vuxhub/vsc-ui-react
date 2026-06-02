60e2ea826fd1be9645d1517aeb6fa58082c79e19
Kham Udom <khamu@outlook.com>
2026-06-02 10:10:50 -0700

    Removed screen import

--- test/VscDivider.test.tsx
+++ test/VscDivider.test.tsx
@@ -1,5 +1,5 @@
 import React, { createRef } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { VscDivider } from '../src';
 
